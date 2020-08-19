7ba636712c9c2ba8d6b04172072a8c4e98f61b76
Rick Belson <belsonrj@aol.com>
2020-08-19 23:31:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ class Tree {
   }
   
   static definition() {
-    return `A pet is an animal kept primarily for a person's company.`;
+    return `A tree is a perennial plant with an elongated stem, or trunk, supporting`;
   }
 }
