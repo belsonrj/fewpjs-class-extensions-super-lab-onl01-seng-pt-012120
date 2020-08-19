09a3bc8b07ca7d47ff1d7425327610aeabbd4b03
Rick Belson <belsonrj@aol.com>
2020-08-19 23:37:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,14 +9,27 @@ class Tree {
 }
 
 class Deciduous extends Tree {
-    constructor(species, name) {
+  constructor(species, name) {
     super(species);
     this.name = name;
   }
   
-    static definition() {
+  static definition() {
     return (
-      super.definition() + ' Dogs are one of the most common types of pets.'
+      super.definition() + '  Deciduous trees shed their leaves annually.'
+    );
+  }
+}
+  
+class Evergreen extends Tree {
+  constructor(species, name) {
+    super(species);
+    this.name = name;
+  }
+  
+  static definition() {
+    return (
+      super.definition() + '  Deciduous trees shed their leaves annually.'
     );
   }
 }
